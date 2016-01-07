7e0dfc20556f6ebb764cd3ddc6e80681f44c45c9
JDarkGreen <jgomez.4net@gmail.com>
2016-01-06 21:33:29 -0500

    subi la estructura a github

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+
+$("#state").change( event => {  
+
+	$.get( `towns/${ event.target.value }` , function( res , sta ) { 
+
+		$("#town").empty();
+
+		res.forEach( element => { 
+			$("#town").append(`<option value=${element.id}> ${ element.name } </option>`);
+		});
+
+	});
+
+
+});
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	@yield('content')
+
+
+	{!! Html::script('js/jquery-1.11.3.min.js') !!}
+	{!! Html::script('js/script.js') !!}
+</body>
+</html>
